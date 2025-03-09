e9cf5d4d02cb945ddec1a0d086e4db839a4843de
solovyovsdom <joelborofsky@mac.com>
2025-03-09 16:11:32 -0500

    Set up TypeScript project with initial files and configurations

---
+++ max-typescript/classes-and-interfaces/basics.js
@@ -0,0 +1,28 @@
+"use strict";
+// class User {
+//   name: string
+//   age: number
+//   // constructor() { // no need for a constructor if it's a fixed property
+//   // this.name = 'Joel' // in Vanilla JS you can just do it this way - but in TS you must add the property a different way
+//   // }
+//   // however, it's rare to have a class that doesn't accept dynamic info, so a constructor is almost always set up, but in typescript it's a bit different
+//   constructor(name: string, age: number) {
+//     this.name = name
+//     this.age = age
+//   }
+// }
+// A short cut so the properties don't have to be created and thus making the code longer
+class User {
+    name;
+    age;
+    // if you add public before a parameter, then TS will make a property of the same name behind the scenes 
+    // it will also assign the value that is recieved automatically to that property 
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+// creating a new user
+const joel = new User('Joel', 41);
+const fred = new User('Fred', 40);
+console.log(joel, fred);
