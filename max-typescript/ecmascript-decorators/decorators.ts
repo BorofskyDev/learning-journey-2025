9146670f99018f94f77c9028ecb9fbb37b69260e
solovyovsdom <joelborofsky@mac.com>
2025-03-12 18:55:38 -0500

    Add initial decorators file for future implementation and reference

---
+++ max-typescript/ecmascript-decorators/decorators.ts
@@ -0,0 +1,4 @@
+// Decorators are for classes and class-heavy frameworks
+// My stack at the moment is Next.js, Prisma (SQL), and MongoDB. 
+// I don't need the decorators for my stack 
+// I plan on coming back to this at the end of my studies since it IS relevant to non-Next.js code
